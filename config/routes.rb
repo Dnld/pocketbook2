5ea6deaa48e8f009a5c26f233cc96140eb6f4574
Donald Steinert <steinert.donald@gmail.com>
2015-03-14 14:12:18 -0400

    completed user controller, create method and view

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'pages/about'
   match 'about', :to => 'pages#about', :via => :get
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
